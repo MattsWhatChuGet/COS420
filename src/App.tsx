b46f47b3e489482d335b53384dd00f53a1556346
Matthew Patterson <matthew.patterson@maine.edu>
2023-09-07 19:42:59 -0400

    Learned that I can see the changes with a file save instead of a commit.
    THe format is getting there for now.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,43 @@
 import React from "react";
 import "./App.css";
-import { Button } from "react-bootstrap";
+import {Button, Col, Container, Row} from "react-bootstrap";
+import myCloseUp from "./Images/MyCloseUp.jpg";
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header style={{ background: "plum" }} className="App-header">
+            <header style={{ background: "purple" }} className="App-header">
+                <img
+                    src={myCloseUp}
+                    width="200"
+                    height="225"
+                    alt="My Close Up."
+                />
                 Matthew Patterson
             </header>
-            <img src="Images/MyCloseUp.jpg" alt="My Close Up."/>
             <h3>New Media & Computer Science</h3>
             <p>COS420 Software Engineering Class of Fall 2023</p>
-            <Button onClick={() => console.log("Hello World")}>
+            <Button onClick={() => console.log("Hello World!")}>
                 Log Hello World
             </Button>
+            <ul>
+                <li>Family Time</li>
+                <li>Board/Video Games</li>
+                <li>Listening to music</li>
+                <li>Philosophy</li>
+                <li>Playing Guitar/Drums/Piano</li>
+            </ul>
+            <Container>
+                <Row>
+                    <Col>Test</Col>
+                    <Col>
+                        1
+                        2
+                        3
+                        4
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
