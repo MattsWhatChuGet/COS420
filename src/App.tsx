90731af5c360b4238f7cceed9593c794e8c2031b
Matthew Patterson <matthew.patterson@maine.edu>
2023-09-07 17:14:49 -0400

    This is the first part of the first assignment's tasks.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
+import {Button} from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">Matthew Patterson</header>
+            <header style={{background:"plum"}} className="App-header">Matthew Patterson</header>
+            <img src="Images/MyCloseUp.jpg" alt="My Close Up."/>
+            <h3>New Media & Computer Science</h3>
             <p>COS420 Software Engineering Class of Fall 2023</p>
+            <Button onClick={() => console.log("Hello World")}>Log Hello World</Button>
         </div>
     );
 }
