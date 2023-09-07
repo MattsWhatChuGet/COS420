f4e18c5f0ebbb4887e034820d985d30413e018b6
Matthew Patterson <matthew.patterson@maine.edu>
2023-09-07 17:22:50 -0400

    Messing with syntax to satisfy `Prettier`'s errors.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import { Button } from "react-bootstrap";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header style={ { background:"plum" }} className="App-header">Matthew Patterson</header>
+            <header style={ { background:"plum" }} className="App-header">
+                Matthew Patterson
+            </header>
             <img src="Images/MyCloseUp.jpg" alt="My Close Up."/>
             <h3>New Media & Computer Science</h3>
             <p>COS420 Software Engineering Class of Fall 2023</p>
